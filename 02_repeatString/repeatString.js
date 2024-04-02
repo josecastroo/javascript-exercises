3856853b8e9ffb3f6b51fe766adb8df17b4e30b4
josecastroo <castrooj02@gmail.com>
2024-04-01 19:29:30 -0600

    Update JavaScript exercises
    01_helloWorld completed
    02_repeatString completed
    03_reverseString completed
    04_removeFromArray completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let chain = "";
+    for (let i = 0; i < num; i++) {
+        chain += string;
+    }
+    return chain;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let arrString = string.split("");
+    let reverseArr = arrString.reverse();
+    let reverseString = "";
 
+    for (let i = 0; i < reverseArr.length; i++) {
+        reverseString += reverseArr[i];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
