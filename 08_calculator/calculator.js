3a4b40dccfc4ca3dd3d0364a92f926c31b56938c
josecastroo <castrooj02@gmail.com>
2024-06-18 15:40:29 -0600

    Update JavaScript exercises
    08_calculator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  } return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    product *= arr[i];
+  } return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+  let fact = 1;
+	for (let i = num; i > 0; i--) {
+    fact *= i;
+  } return fact;
 };
 
 // Do not edit below this line
