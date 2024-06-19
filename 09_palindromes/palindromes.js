25fa3037b089397f7bfa70cfcd36aee1dd9c7b44
josecastroo <castrooj02@gmail.com>
2024-06-19 16:48:33 -0600

    Update JavaScript exercises
    09_palindromes completed
    10_fibonacci completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let cleanedString = string.toLowerCase().split('').filter((character) => alphanumerical.includes(character)).join('');
+    let reversedString = cleanedString.split('').reverse().join('');
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+    let count;
+    if (typeof pos !== 'number') {
+        count = parseInt(pos);
+    } else {
+        count = pos;
+    }
+    if (count == 1 || count == 0) {
+        return count;
+    } else if (count < 0) {
+        return "OOPS";
+    } else {
+        let fibo = [0, 1];
+        for (let i = 2; i <= pos; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        } return fibo[count];
+    }
 };
 
 // Do not edit below this line
